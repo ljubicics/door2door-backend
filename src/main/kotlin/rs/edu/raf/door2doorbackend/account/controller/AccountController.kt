ca5a1ded110da54d7fe2c1e1ea566ec7adb9714c
Strahinja Ljubicic <strahinja.ljubicic@gmail.com>
2024-08-28 15:28:47 +0200

    Add cors to account controller

--- src/main/kotlin/rs/edu/raf/door2doorbackend/account/controller/AccountController.kt
+++ src/main/kotlin/rs/edu/raf/door2doorbackend/account/controller/AccountController.kt
@@ -2,12 +2,10 @@ package rs.edu.raf.door2doorbackend.account.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import rs.edu.raf.door2doorbackend.account.service.AccountService
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/accounts")
 class AccountController(
